c104d9f8f370438598cf8e3489109c25c1baae3f
Pierre Allard <palla036@uottawa.ca>
2024-10-20 14:05:20 -0400

    i have no clue

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+      apiKey: "AIzaSyDscd6oqmH5qYig5H3htxXQOmnMf3ki7To",
+      authDomain: "lab5-1515c.firebaseapp.com",
+      projectId: "lab5-1515c",
+      storageBucket: "lab5-1515c.appspot.com",
+      messagingSenderId: "998311170766",
+      appId: "1:998311170766:web:0446b04d23623ebe78369f"
+  }
+};
